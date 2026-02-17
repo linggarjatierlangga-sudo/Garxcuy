396e2e37a8e717882f22761befc73576f2932d29
linggarjatierlangga-sudo <linggarjatierlangga@gmail.com>
2026-02-17 18:34:32 +0700

    Rename hub to GARXCUY and change theme

--- GarxcuyHub.lua
+++ GarxcuyHub.lua
@@ -1,6 +1,6 @@
 -- Script Fix by ShadowX
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-local Window = Library.CreateLib("Double Example Hub", "DarkTheme")
+local Window = Library.CreateLib("GARXCUY HUB", "Synapse")
 
 -- Main Tab
 local MainTab = Window:NewTab("Main")
